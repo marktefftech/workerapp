435444cc5c69fdc35b884d739d5a6a9db6ed3d7c
Joe Stowers <greenchiletexan@gmail.com>
2019-04-11 20:23:14 -0500

    project_list: add Client class and fix date display

--- lib/src/project_list.dart
+++ lib/src/project_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Projectlist extends StatefulWidget {
   @override
@@ -10,13 +11,12 @@ class Projectlist extends StatefulWidget {
 }
 
 class ProjectlistState extends State<Projectlist> {
-  final _suggestions = <String>[];
-
+  final _clients = <Client>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
-    _suggestions.removeRange(0, _suggestions.length);
+    _clients.removeRange(0, _clients.length);
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection("projectlists").snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -25,41 +25,70 @@ class ProjectlistState extends State<Projectlist> {
           case ConnectionState.waiting:
             return new Center(child: new LinearProgressIndicator());
           default:
-            print("data start");
-            print(snapshot.data.documents);
             for (int i = 0; i <= snapshot.data.documents.length - 1; i++) {
-              if (snapshot.data.documents[i].data.containsKey("clientname")) {
-                _suggestions.add(snapshot.data.documents[i].data["clientname"]);
-              }
+           
+              Client client = new Client();
+              client.validateClientData(snapshot.data.documents[i].data);
+              _clients.add(client);
+
             }
             return Scaffold(
               appBar: AppBar(
-                title: Text('Startup Name Generator 2.0'),
+                title: Text('Moving Projects List'),
               ),
-              body: _buildSuggestions(),
+              body: _buildClientRows(),
             );
         }
       },
     );
   }
 
-  Widget _buildSuggestions() {
+  Widget _buildClientRows() {
     return new ListView.builder(
-      itemCount: _suggestions.length,
+      itemCount: _clients.length,
       padding: const EdgeInsets.all(10.0),
       itemBuilder: (context, i) {
         // If you've reached the end of the available word pairings...
-        return _buildRow(_suggestions[i]);
+        return _buildRow(_clients[i]);
       },
     );
   }
 
-  Widget _buildRow(String pair) {
+  Widget _buildRow(Client clientOrder) {
     return new ListTile(
       leading: new Icon(Icons.event_seat),
-      title: new Text(pair, style: _biggerFont),
-      subtitle: new Text('Project Date'),
+      title: new Text(clientOrder.name, style: _biggerFont),
+      subtitle: new Text(clientOrder.getDateAsString()),
       onTap: (){},
     );
+  }
+}
+
+class Client {
+  String name;
+  Timestamp date;
+  int installers;
+  int movers;
+
+  String getDateAsString() {
+    if(this.date == null) {
+      return "Missing";
+    }
+    Timestamp timestamp = this.date;
+    var d = new DateFormat.yMMMd().format(
+              new DateTime.fromMillisecondsSinceEpoch(
+              timestamp.millisecondsSinceEpoch));
+    return d;
+  }
+
+  validateClientData(Map<String, dynamic> data) {
+    if (data.containsKey("clientname")) {
+      name = data["clientname"];
+    }
+    if (data.containsKey("date")) {
+      date = data["date"];
+    }
+    installers = data["installers"];
+    movers = data["movers"];
   }
 }
