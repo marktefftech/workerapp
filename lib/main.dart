a7939104a3cfee3488bd524901d27aa4e63232d6
john.jvtran <john.jvtran@gmail.com>
2019-07-24 18:36:19 -0500

    main: change homepage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:worker_app/src/project_list.dart';
-//import 'package:worker_app/src/homepage.dart';
+import 'package:worker_app/src/homepage.dart';
 
 void main() => runApp(App());
 
@@ -13,7 +13,8 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.cyan,
       ),
-      home: Projectlist(),
+      //home: Projectlist(),
+      home: HomePage(),
     );
   }
 }
