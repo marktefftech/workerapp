72be9047e0e7526991a0231d552d3cf657c2d34d
Joe Stowers <greenchiletexan@gmail.com>
2020-01-08 18:55:00 -0600

    Add Google map

--- lib/widgets/city_map.dart
+++ lib/widgets/city_map.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CityMap extends StatelessWidget {
-
   final Completer<GoogleMapController> _controller = Completer();
 
   static const LatLng _center = const LatLng(30.2672, -97.7431);
@@ -12,6 +11,12 @@ class CityMap extends StatelessWidget {
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
+
+  // TODO
+  // 1. retrieve list of projects from Google Firebase
+  // 2. map over lat/lng and display markers
+  // 3. for each marker, display clientName, address, date
+  // 4. cluster annotations like Apple requirements
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +34,12 @@ class CityMap extends StatelessWidget {
           ),
           markers: {
             Marker(
-              markerId: MarkerId('testMarker'),
-              position: LatLng(30.265433, -97.733376),
-              infoWindow: InfoWindow(
-                title: 'Info Window Title'
-              ),
-            )
+                markerId: MarkerId('testMarker'),
+                position: LatLng(30.265433, -97.733376),
+                infoWindow: InfoWindow(title: 'Info Window Title'),
+                onTap: () {
+                  print('Hello');
+                })
           },
         ),
       ),
